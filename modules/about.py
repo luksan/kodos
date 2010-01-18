31142a5fe621384c6c83c3f5df2122e209f898fe
Lukas Sandström <luksan@gmail.com>
2010-01-18 13:28:09 +0100

    Remove getPixmap use from about.py
    The large Kodos logo is set as a resource in the
    ui file now, so we don't need getPixmap here anymore.
    
    Signed-off-by: Lukas Sandström <luksan@gmail.com>

--- modules/about.py
+++ modules/about.py
@@ -1,12 +1,11 @@
+# -*- coding: utf-8 -*-
 #  about.py: -*- Python -*-  DESCRIPTIVE TEXT.
 
 from aboutBA import *
-from util import getPixmap
 import version
 
 class About(AboutBA):
     def __init__(self):
         AboutBA.__init__(self)
-        self.image0 = getPixmap("kodos.png")
         self.versionLabel.setText(version.VERSION)
 
