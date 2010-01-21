bb9ab13f74e5b39a2451368997ac8e64ee5e27e6
Lukas Sandström <luksan@gmail.com>
2010-01-21 15:46:59 +0100

    Delete unused file TimeoutError.py
    Signed-off-by: Lukas Sandström <luksan@gmail.com>

--- modules/TimeoutError.py
+++
@@ -1,8 +0,0 @@
-
-class TimeoutError(Exception):
-    def __init__(self, value=None):
-        self.value = value
-
-    def __str__(self):
-        "Return a string representation of the exception"
-        return str(self.value)
